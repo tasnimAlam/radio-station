a13da7ea87cba7d234aacd99a16de9dc628571f9
Md. Tasnim Alam <tasnimalamcse@gmail.com>
2022-05-08 21:14:08 +0600

    Add scss modules

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import "./App.css";
+import "./App.scss";
 import { RootState } from "./app/store";
 import { StationHeader } from "./components/StationHeader/StationHeader";
 
 function App() {
-  const stations = useSelector((state: RootState) => state.stations.names);
+	const stations = useSelector((state: RootState) => state.stations.names);
 
-  return (
-    <div className="appWrapper">
-      <StationHeader />
-    </div>
-  );
+	return (
+		<div className="appWrapper">
+			<StationHeader />
+		</div>
+	);
 }
 
 export default App;
--- src/components/StationHeader/StationHeader.tsx
+++ src/components/StationHeader/StationHeader.tsx
@@ -1,11 +1,12 @@
 import React from "react";
+import styles from "./StationHeader.module.scss";
 
 export const StationHeader = () => {
-  return (
-    <div className="stationHeader">
-      <img src="back-arrow.png" />
-      <h2>Stations</h2>
-      <img src="switch.png" />
-    </div>
-  );
+	return (
+		<div className={styles.stationHeader}>
+			<img src="back-arrow.png" />
+			<h2>Stations</h2>
+			<img src="switch.png" />
+		</div>
+	);
 };
