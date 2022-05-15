b98576c557ef179157fb1a4607365c5fac864731
tasnimAlam <tasnimalamcse@gmail.com>
2022-05-15 23:21:03 +0600

    Add no operations click functions

--- src/components/StationCard/StationCard.tsx
+++ src/components/StationCard/StationCard.tsx
@@ -2,11 +2,24 @@ import React from "react";
 import styles from "./StationCard.module.scss";
 
 export const StationCard = () => {
+  const onMinusClick = () => {};
+  const onPlusClick = () => {};
+
   return (
     <div className={styles.container}>
-      <img className={styles.minusIcon} src="minus.png" alt="minus-logo" />
-      <img className={styles.fmLogo} src="fm_logo.svg" alt="fm-logo" />
-      <img className={styles.plusIcon} src="plus.png" alt="plug-logo" />
+      <img
+        className={styles.minusIcon}
+        src="minus.png"
+        alt="minus-logo"
+        onClick={() => onMinusClick()}
+      />
+      <img className={styles.fmLogo} src="fm-logo.svg" alt="fm-logo" />
+      <img
+        className={styles.plusIcon}
+        src="plus.png"
+        alt="plug-logo"
+        onClick={() => onPlusClick()}
+      />
     </div>
   );
 };
--- src/components/StationHeader/StationHeader.tsx
+++ src/components/StationHeader/StationHeader.tsx
@@ -2,11 +2,24 @@ import React from "react";
 import styles from "./StationHeader.module.scss";
 
 export const StationHeader = () => {
+  const onBackArrowClick = () => {};
+  const onSwitchClick = () => {};
+
   return (
     <div className={styles.header}>
-      <img className={styles.icon} src="back-arrow.png" alt="back-arrow" />
+      <img
+        className={styles.icon}
+        src="back-arrow.png"
+        alt="back-arrow"
+        onClick={() => onBackArrowClick()}
+      />
       <h2 className={styles.title}>STATIONS</h2>
-      <img className={styles.icon} src="switch.png" alt="back-arrow" />
+      <img
+        className={styles.icon}
+        src="switch.png"
+        alt="switch"
+        onClick={() => onSwitchClick()}
+      />
     </div>
   );
 };
